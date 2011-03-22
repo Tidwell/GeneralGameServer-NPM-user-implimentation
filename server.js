c51a8c17edfd06593a4d03bfe288d58c17b61974
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2011-03-22 19:08:50 +0000

    Moved require out of gamestate module itself, now does inheratance in the framework itself

--- server.js
+++ server.js
@@ -1,29 +1,46 @@
 var log = require('logging');
 
 var game = require('generalGameServer').createServer({
-  /*  STRING
-      Path to where all the front-end code lives (html/css/js)
-      relative to the ./, without leading or trailing /
-      default: 'client'
-  */
-  clientDir: 'client',
-  /*
-      BOOL
-      If the generalGameServer should serve http requests.  You 
-      can replace the generalGameServer http server with
-      Any other http server (express, apache, nginx) and point the 
-      webroot to ./client
-      default: true
-  */
-  serveHttp: true
+  server: {
+    /*  STRING
+        Path to where all the front-end code lives (html/css/js)
+        relative to the ./, without leading or trailing /
+        default: 'client'
+    */
+    clientDir: 'client',
+    /*
+        BOOL
+        If the generalGameServer should serve http requests.  You 
+        can replace the generalGameServer http server with
+        Any other http server (express, apache, nginx) and point the 
+        webroot to ./client
+        default: true
+    */
+    serveHttp: true,
+  },
+  game: {
+    //# or infinite
+    instances: 'infinite',
+    userCreated: true,
+    //# or infinite
+    minPlayers: 2,
+    //# or infinite
+    maxPlayers: 2 
+  }
 });
+
 /*
+game.modules                all instantiated module objects
+game.gamestateTemplate      the constructor used to create new gamestates, call with
+                                var g = new obj.gamestateTemplate inside of any module
+
+
+
 game.modules.auth.init({
   type: ['anonymous', 'anonymous/named', 'authenticated'],
   callback: function() { console.log('auth init') }
 })
 game.modules.matchmaker.init({
-  type: ['random', 'private', 'single-world', 'multiple-world'],
   callback: function() { console.log('matchmaker init') }
 })
 game.modules.gameevent.init({
